93f68b9cce48f5acd9c9a59a2f1affc96786f24d
SaraVieira <hey@iamsaravieira.com>
2024-07-24 11:07:19 +0100

    fix ts again

--- scraper/utils.js
+++ scraper/utils.js
@@ -1,4 +1,5 @@
 import { DateTime } from "luxon";
+import { LOG_ICON } from "./consts.js";
 
 export function log(level, message) {
   console.log(
@@ -21,7 +22,7 @@ export function progressBar(title, count, total) {
 }
 
 export function sanitizeText(text) {
-  return text
+  return (text || "")
     .replace(/\n\r|\r\n|\r \n|\r|\n|\t/g, " ")
     .replace("&quot;", "'")
     .replace(/(https|http):\/\/(\w|\.|\/|\?|\=|\&|\%)*\b/g, "")
@@ -31,5 +32,51 @@ export function sanitizeText(text) {
 }
 
 export function priceToFloat(price, decimals = 2) {
-  return parseFloat(price.replace(",", ".")).toFixed(decimals);
+  return parseFloat(price.replace("$", "").replace(",", ".")).toFixed(decimals);
 }
+
+export const getLanguages = (app) => {
+  let full_audio_languages = [];
+  let supported_languages = [];
+  if (app.supported_languages) {
+    let languagesApp = app.supported_languages
+      .replace(/<[^<]+?>/g, "")
+      .replace("languages with full audio support", "");
+    languagesApp.split(", ").map((lang) => {
+      lang = lang.replace("*", "");
+      if (lang.includes("*")) {
+        full_audio_languages.push(lang);
+      }
+      supported_languages.push(lang);
+    });
+  }
+
+  return { full_audio_languages, supported_languages };
+};
+
+export const getGamePkgs = (app) => {
+  let packages = [];
+  if (app.package_groups) {
+    app.package_groups.forEach((pkg) => {
+      let subs = [];
+      if (pkg.subs) {
+        pkg.subs.forEach((sub) => {
+          subs.push({
+            text: sanitizeText(sub.option_text),
+            description: sub.option_description,
+            price: (
+              parseFloat(sub.price_in_cents_with_discount) * 0.01
+            ).toFixed(2),
+          });
+        });
+      }
+      packages.push({
+        title: sanitizeText(pkg.title),
+        description: sanitizeText(pkg.description),
+        subs,
+      });
+    });
+  }
+
+  return packages;
+};
