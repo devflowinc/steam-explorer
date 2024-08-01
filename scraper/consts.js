ec773b3e36cfe8e8764538ed44750f5c16b3999c
SaraVieira <hey@iamsaravieira.com>
2024-08-01 17:35:51 +0100

    assorted fixes

--- scraper/consts.js
+++ scraper/consts.js
@@ -1,6 +1,8 @@
 export const DEFAULT_SLEEP = 1.5;
 export const DEFAULT_RETRIES = 4;
 export const DEFAULT_TIMEOUT = 10000;
+export const DEFAULT_CURRENCY = "us";
+export const DEFAULT_LANGUAGE = "en";
 export const LOG_ICON = ["i", "W", "E", "!"];
 export const INFO = 0;
 export const WARNING = 1;
