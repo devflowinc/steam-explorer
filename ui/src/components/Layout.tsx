b52096af0af64e579876b57b9206fbad2d91d703
SaraVieira <hey@iamsaravieira.com>
2024-08-12 20:24:31 +0100

    add subtitle

--- ui/src/components/Layout.tsx
+++ ui/src/components/Layout.tsx
@@ -7,6 +7,9 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
       <header className="my-12 flex justify-center">
         <Link to={"/"}>
           <Logo className="w-[300px] h-auto" />
+          <h1 className="font-thin italic text-center mt-2">
+            when search isn't enough
+          </h1>
         </Link>
       </header>
       <div className="container my-12"> {children}</div>
